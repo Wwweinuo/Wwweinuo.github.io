21dc3e74961ff9f98e5b5c8926fc375fcc2c9b03
Wwweinuo <2992180144@qq.com>
2026-06-11 23:19:18 +0800

    feat: 添加回到顶部按钮（BackToTop）

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,18 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import HomePage from './components/HomePage.vue'
+import BackToTop from './components/BackToTop.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('HomePage', HomePage)
+  },
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'layout-bottom': () => h(BackToTop),
+    })
   },
 } satisfies Theme
